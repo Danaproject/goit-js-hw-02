c5afffe0c887022fd004a6e6c983e97b1759cef1
Dana Project <70840490+Danaproject@users.noreply.github.com>
2020-11-29 20:04:27 +0100

    +task-05 another solution

---
+++ js/task-05-1.js
@@ -0,0 +1,8 @@
+const checkForSpam = function(message) {
+    const regex = /sale|spam/i;
+    return message.match(regex) ? true : false;
+};
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
